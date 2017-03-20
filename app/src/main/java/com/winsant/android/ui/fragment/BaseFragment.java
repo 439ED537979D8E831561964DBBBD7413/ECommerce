33447823cf2c6e92257f9275d782809c469eb5f0
Ashish Dungrani <hardikdungrani8@gmail.com>
2017-03-20 12:39:03 +0530

    Commit on 20 March 2017 12:38 PM

--- app/src/main/java/com/winsant/android/ui/fragment/BaseFragment.java
+++ app/src/main/java/com/winsant/android/ui/fragment/BaseFragment.java
@@ -17,7 +17,6 @@ public class BaseFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         activity = (HomeActivity) getActivity();
     }
 }
